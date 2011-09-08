5fb55c7a44879b9e0f5a531312edcfc0fc75a8bb
mpapis <mpapis@gmail.com>
2011-09-08 21:05:02 +0200

    require sm_local_path

--- prepare.sh
+++ prepare.sh
@@ -3,6 +3,12 @@
 set -o errexit
 set -o nounset
 
+if [[ -z "$sm_local_path" || ! -d "$sm_local_path" ]]
+then
+  printf "sm_local_path is not pointing on a direcotory.\n"
+  exit 1
+fi
+
 export initial_pwd="$PWD"
 export sm_path="$sm_local_path"
 export modules_path="${sm_path}/core/sm/shell"
