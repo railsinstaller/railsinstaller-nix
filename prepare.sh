4fff556ba33bcb3df595963072583fffd80389ce
mpapis <mpapis@gmail.com>
2011-09-08 18:47:46 +0200

    use local sm checkout for scripting

---
+++ prepare.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+
+export initial_pwd="$PWD"
+export sm_path="$sm_local_path"
+export modules_path="${sm_path}/core/sm/shell"
+export sm_script=/
+export extensions_search_paths=( $PWD/sm )
+
+. "${modules_path}/core/cli"
+. "${modules_path}/core/initialize"
+
+modules rix
+
+rix_start
